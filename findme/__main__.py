ce0a58ac1d21902e17d65525c54dbf6fa52bed84
mdLafrance <maxlafrance97@gmail.com>
2023-08-25 15:28:21 -0400

    CLI working

--- findme/__main__.py
+++ findme/__main__.py
@@ -1,13 +1,18 @@
 """CLI entrypoint for findme."""
+import os
 import sys
 import argparse
 
+from typing import List, Any
 from textwrap import dedent
 from enum import Enum
 
-from rich.markdown import Markdown
+from rich import print as rprint
+from rich.table import Table
 
-from rich_argparse import RawDescriptionRichHelpFormatter
+from findme.config import Pattern, load_config, save_config
+from findme.search import find_pattern
+from findme.exceptions import DuplicateAliasError
 
 
 DESCRIPTION_STRING = """
@@ -43,52 +48,132 @@ class Commands(str, Enum):
     CONFIG = "config"
 
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("pattern_name", nargs="?", help="Pattern(s) to search for.")
+
+    parser.add_argument("search_root", nargs="?", help="Directory to search in (defaults to current directory.)")
+
+    parser.add_argument("-a", "--add", help="Alias for pattern to add. Must be used in conjunction with --pattern.")
+    parser.add_argument("-p", "--pattern", help="Regex pattern to be added for the given alias.")
+    parser.add_argument("-f", "--files-only", action="store_true", help="Only match this pattern against files.")
+    parser.add_argument("-d", "--directories-only", action="store_true", help="Only match this pattern against directories.")
+
+    parser.add_argument("-r", "--remove", help="Remove the given alias from the config.")
+
+    parser.add_argument("-l", "--list", action="store_true", help="List current patterns.")
+
+    args = parser.parse_args()
+
+    if args.add and not args.pattern:
+            parser.error("Must use --add in conjunction with --pattern.")
+
+    return args
+
+
 def app():
-    show_help = "-h" in sys.argv or "--help" in sys.argv
-    no_commands = len(sys.argv) == 1
-    search_for_pattern = len(sys.argv) > 1 and (sys.argv[1] not in [e.value for e in Commands])
+    ### Parse cli args
+    args = parse_args()
 
-    if show_help and search_for_pattern:
-        sys.argv = [sys.argv[0], "--help"]
+    ### --list 
+    if args.list:
+        list_patterns()
 
-    add_subparsers = show_help or no_commands or not search_for_pattern
+    ### --add
+    if args.add:
+        add_pattern(args.add, args.pattern, args.files_only, args.directories_only)
 
-    print("Search for pattern?", search_for_pattern)
-    print("add subparsers?", add_subparsers)
+    ### --remvoe
+    if args.remove:
+        remove_pattern(args.remove)
 
-    ### Parse command line args 
-    parser = argparse.ArgumentParser(description=DESCRIPTION_STRING, formatter_class=RawDescriptionRichHelpFormatter)
+    ### Search for pattern
+    search_for_pattern(args.pattern_name, args.search_root)
 
-    if not add_subparsers:
-        parser.add_argument("pattern", nargs='?')
-    
+
+def search_for_pattern(alias: str, search_root: str):
+    patterns = _try_load_config()
+
+    pattern_to_search_for = next((p for p in patterns if p.alias == alias), None)
+
+    if not pattern_to_search_for:
+        _print_error(f"Pattern {alias} not found")
+        sys.exit(1)
+
+    for match in find_pattern(search_root or os.getcwd(), pattern_to_search_for):
+        print(match)
+
+    sys.exit(0)
+
+
+def list_patterns():
+    patterns = _try_load_config()
+
+    if not patterns:
+        print("No patterns saved in config.")
     else:
-        command_subparser = parser.add_subparsers(dest="command")
+        _print_patterns_in_table(patterns)
 
-        # Parser for add command
-        add_parser = command_subparser.add_parser("add", help="Add a pattern.", formatter_class=RawDescriptionRichHelpFormatter)
-        add_parser.add_argument("pattern", help="Name of the pattern to add.")
-        add_parser.add_argument("regex", help="Regex for this pattern.")
-        add_parser.add_argument("-o", "--overwrite", action="store_true", help="Overwrite the pattern if it already exists.")
+    sys.exit(0)
 
-        # Parser for remove command
-        remove_parser = command_subparser.add_parser("remove", help="Remove a pattern.", formatter_class=RawDescriptionRichHelpFormatter)
-        remove_parser.add_argument("pattern", help="Name of the pattern to remove.")
-        
-        # Parser for list command
-        list_parser = command_subparser.add_parser("list", help="List patterns.", formatter_class=RawDescriptionRichHelpFormatter)
 
-    parser.parse_args()
+def add_pattern(alias: str, pattern: str, files_only: bool, directories_only: bool):
+    patterns = _try_load_config()
 
-    # Exit with help message if no args supplied
-    if len(sys.argv) == 1:
-        parser.print_help(sys.stderr)
+    new_pattern = Pattern(alias=alias, pattern=pattern, files_only=files_only, directories_only=directories_only)
+
+    patterns.append(new_pattern)
+
+    try:
+        save_config(patterns)
+    except DuplicateAliasError as e:
+        _print_error(str(e))
+        sys.exit(1)
+
+    print("Added the following pattern:")
+    _print_patterns_in_table([new_pattern])
+
+    sys.exit(0)
+
+
+def remove_pattern(alias: str):
+    patterns = _try_load_config()
+
+    pattern_to_remove = next((p for p in patterns if p.alias == alias), None)
+
+    if pattern_to_remove:
+        patterns.remove(pattern_to_remove)
+        save_config(patterns)
         sys.exit(0)
+    else:
+        _print_error(f"Pattern {alias} not found in config.")
+        sys.exit(1)
 
-    # Determine whether the supplied arg is a valid command, or a user defined alias
-    command = sys.argv[1]
 
-    print(parser.parse_args())
+def _print_patterns_in_table(patterns: List[Pattern]):
+    table = Table("Pattern name", "Pattern", "Files only?", "Directories only?")
+
+    for pattern in patterns:
+        table.add_row(pattern.alias, pattern.pattern, _color_string(pattern.files_only), _color_string(pattern.directories_only))
+
+    rprint(table)
+
+
+def _try_load_config():
+    try:
+        return load_config()
+    except FileNotFoundError:
+        return []
+
+
+def _color_string(m: Any) -> str:
+    return f"[{'green' if m else 'red'}]{str(m)}[/]"
+
+
+def _print_error(message: str):
+    rprint(f"[bold red]ERROR: [/]{message}")
+     
 
 
 if __name__ == "__main__":
